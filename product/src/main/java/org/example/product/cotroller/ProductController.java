e501c6e48bcce3694439faa696c4fa2802d408b3
Lee <why48382@gmail.com>
2025-08-06 12:23:43 +0900

    [Feat] 컨트롤러 기능 추가

--- product/src/main/java/org/example/product/cotroller/ProductController.java
+++ product/src/main/java/org/example/product/cotroller/ProductController.java
@@ -1,19 +1,36 @@
 package org.example.product.cotroller;
 
+import org.example.product.model.ProductDto;
+import org.example.product.service.ProductService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-    private final ProductService productservice;
+
+    private final ProductService productService;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @PostMapping("/save")
-    public ResponseEntity productSave(@RequestBody ProductDto.save dto) {
-        productservice.save(dto);
+    public ResponseEntity productSave(@RequestBody ProductDto.Register dto) {
+        productService.register(dto);
         return ResponseEntity.status(200).body("저장 성공");
+    }
+
+    @GetMapping("/read")
+    public ResponseEntity productRead(Integer idx) {
+        ProductDto.product result = productService.read(idx);
+        return ResponseEntity.status(200).body(result);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity productList() {
+        List<ProductDto.product> list = productService.list();
+        return ResponseEntity.status(200).body(list);
     }
 }
