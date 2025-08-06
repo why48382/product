4ab1b85c4cdc544c92a02340413844a52897a2ed
Lee <why48382@gmail.com>
2025-08-06 16:40:43 +0900

    [Feat] movie service 제작

---
+++ product/src/main/java/org/example/movie/service/MovieService.java
@@ -0,0 +1,33 @@
+package org.example.movie.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.movie.model.MovieEntity;
+import org.example.movie.repository.MovieRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MovieService {
+    private final MovieRepository movieRepository;
+
+    public void save(MovieDto.resister dto) {
+        movieRepository.save(dto.toEntity());
+    }
+
+    public MovieDto.movie read(Integer id) {
+        Optional<MovieEntity> result = movieRepository.findById(id);
+        if (result.isPresent()) {
+            MovieEntity entity = result.get();
+            return MovieDto.movie.from(entity);
+        }
+        return null;
+    }
+
+    public List<MovieDto.movie> list() {
+        List<MovieEntity> result = movieRepository.findAll();
+        return result.stream().map(MovieDto.movie::from).toList();
+    }
+}
