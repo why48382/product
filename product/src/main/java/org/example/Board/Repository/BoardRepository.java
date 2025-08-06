cd17ee8a8dccb15b35cc5a3ad284f97e987d98f0
Lee <why48382@gmail.com>
2025-08-06 14:49:56 +0900

    패키지 경로 재설정

--- product/src/main/java/org/example/Board/Repository/BoardRepository.java
+++ product/src/main/java/org/example/Board/Repository/BoardRepository.java
@@ -1,13 +1,11 @@
-package org.example.Board.Repository;
+package org.example.board.Repository;
 
-import org.example.product.Product.model.ProductEntity;
+import org.example.board.model.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface BoardRepository extends JpaRepository<ProductEntity, Integer> {
-    List<ProductEntity> findByName(String name);
-    List<ProductEntity> findByDetail(String detail);
-    List<ProductEntity> findByPrice(Integer price);
+public interface BoardRepository extends JpaRepository <BoardEntity, Integer>{
+List<BoardEntity> findByTitle(String title);
 
 }
